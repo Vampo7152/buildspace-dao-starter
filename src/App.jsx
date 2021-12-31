0f4ab3e5d8ba63bca69cbb2ab25dbe2277006f46
umang <umangboss786@gmail.com>
2021-12-31 18:41:03 +0530

    Title

--- src/App.jsx
+++ src/App.jsx
@@ -379,7 +379,7 @@ if (hasClaimedNFT) {
   // Render mint nft screen.
   return (
     <div className="mint-nft">
-      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <h1>Mint your free 🍪DAO Membership NFT, LFG!</h1>
       <button
         disabled={isClaiming}
         onClick={() => mintNft()}
